77422e1b51ce5090dce61106ce52d6d817b5d54b
tommady <tommady@users.noreply.github.com>
2019-10-19 21:29:07 +0800

    change into accepting variables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::env;
+extern crate structopt;
+use structopt::StructOpt;
 
-fn make_text(args: Vec<String>) -> String {
-    let text = args.join(" ");
-
+fn make_text(text: String, from: String, to: String, to_lowercase: bool) -> String {
     let out: String = text
         .chars()
         .map(|x| match x {
@@ -10,18 +9,20 @@ fn make_text(args: Vec<String>) -> String {
             '.' => ' ',
             '/' => ' ',
             ',' => ' ',
+            '!' => ' ',
             _ => x,
         })
         .collect();
 
-    out.split_whitespace()
-        .collect::<Vec<&str>>()
-        .join("-")
-        .to_lowercase()
-}
+    let str_from = from.to_string();
+    let str_to = to.to_string();
+    let mut ret = out.split(&str_from).collect::<Vec<&str>>().join(&str_to);
 
-macro_rules! vec_of_strings {
-    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    if to_lowercase {
+        ret = ret.to_lowercase();
+    }
+
+    ret
 }
 
 #[cfg(test)]
@@ -31,26 +32,49 @@ mod tests {
     #[test]
     fn test_make_text() {
         let want = String::from("gglong-ggsmall");
-
-        let got = make_text(vec_of_strings!["gglong", "ggsmall"]);
+        let got = make_text(
+            String::from("gglong_ggsmall"),
+            String::from("_"),
+            String::from("-"),
+            false,
+        );
         assert_eq!(want, got);
 
-        let got = make_text(vec_of_strings!["gglong/.ggsmall"]);
+        let want = String::from("gglong-ggsmall");
+        let got = make_text(
+            String::from("GGLONG_GGSMALL"),
+            String::from("_"),
+            String::from("-"),
+            true,
+        );
         assert_eq!(want, got);
 
-        let got = make_text(vec_of_strings!["gglong?./ggsmall"]);
-        assert_eq!(want, got);
-
-        let got = make_text(vec_of_strings!["GglonG?./GGSMALL"]);
+        let want = String::from("gglong\\ ggsmall");
+        let got = make_text(
+            String::from("gglong ggsmall"),
+            String::from(" "),
+            String::from("\\ "),
+            true,
+        );
         assert_eq!(want, got);
     }
 }
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "better_be", about = "format the words you want")]
+struct Opt {
+    #[structopt(parse(from_str))]
+    input_words: String,
+    #[structopt(short = "f", long = "from", default_value = " ")]
+    words_from: String,
+    #[structopt(short = "t", long = "to", default_value = "\\ ")]
+    words_to: String,
+    #[structopt(short, long)]
+    lowercase: bool,
+}
+
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-
-    args = args[1..].to_vec();
-    let out = make_text(args);
-
+    let opt = Opt::from_args();
+    let out = make_text(opt.input_words, opt.words_from, opt.words_to, opt.lowercase);
     println!("{}", out);
 }
